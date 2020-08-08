de782f8dc28f80a5af37294687d2d3da1609d75e
Arthur Silva <artrogeno@gmail.com>
2020-08-08 00:19:52 -0300

    feat: add log endpoint on server

--- packages/server/src/api/log/controller.ts
+++ packages/server/src/api/log/controller.ts
@@ -2,10 +2,19 @@ import { Request as Req, Response as Res, NextFunction as Next } from 'express'
 
 import LogEntry from '../../shared/models/logEntry'
 
-export const index = async (req: Req, res: Res, next: Next) => {
+export const index = async (req: Req, res: Res, next: Next): Promise<any> => {
   try {
-    const entries = await LogEntry.find()
+    const entries = await LogEntry.find().select('-__v')
     return res.json(entries)
+  } catch (error) {
+    next(error)
+  }
+}
+export const show = async (req: Req, res: Res, next: Next): Promise<any> => {
+  try {
+    const { id: _id } = req.params
+    const entry = await LogEntry.findOne({ _id }).select('-__v').exec()
+    return res.json(entry)
   } catch (error) {
     next(error)
   }
@@ -14,6 +23,29 @@ export const index = async (req: Req, res: Res, next: Next) => {
 export const store = async (req: Req, res: Res, next: Next): Promise<any> => {
   try {
     const logEntry = await LogEntry.create(req.body)
+    return res.json(logEntry)
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const update = async (req: Req, res: Res, next: Next): Promise<any> => {
+  try {
+    const {
+      params: { id },
+      body,
+    } = req
+    const logEntry = await LogEntry.updateOne({ _id: id }, body)
+    return res.json(logEntry)
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const destroy = async (req: Req, res: Res, next: Next): Promise<any> => {
+  try {
+    const { id: _id } = req.params
+    const logEntry = await LogEntry.deleteOne({ _id })
     return res.json(logEntry)
   } catch (error) {
     next(error)
--- packages/server/src/api/log/index.ts
+++ packages/server/src/api/log/index.ts
@@ -6,5 +6,8 @@ const routes = Router()
 
 routes.get('/', controller.index)
 routes.post('/', controller.store)
+routes.get('/:id', controller.show)
+routes.put('/:id', controller.update)
+routes.delete('/:id', controller.destroy)
 
 export default routes
